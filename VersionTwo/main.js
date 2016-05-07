3452b8d127456ead4341b081f47940b04fac07f9
Peyton Caseria <PeytonCaseria@gmail.com>
2016-05-07 16:57:57 -0600

    VersionOne and VersionTwo PaleGems and Easter Treats

---
+++ VersionTwo/main.js
@@ -0,0 +1,12 @@
+var $accordion = $("#accordion");
+
+$accordion.accordion();
+
+$(".opener").on("click", function () {
+    var $this = $(this),
+        toOpen = $this.panel("data");
+
+    $accordion.accordion("option", "active", toOpen);
+
+    return false;
+});
